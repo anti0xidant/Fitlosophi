fb833276fb2b41f27830f1a786e517cfe6ac33de
koshin abdi <kabdi8@gmail.com>
2015-12-01 21:43:48 -0500

    added post, page, category, tag classes

---
+++ FITernitiMVC/Models/StaticPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FITernitiMVC.Models
+{
+    public class StaticPage
+    {
+        public int PageID { get; set; }
+        public string ButtonName { get; set; }
+        public string Body { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DatePublished { get; set; }
+        public bool IsActive { get; set; }
+        public string WriterName { get; set; }
+
+  
+    }
+}
