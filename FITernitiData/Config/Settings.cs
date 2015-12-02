2fe8f6ff1d4c0a96f26c965dcf7846fb4cb50153
Andrew Tang <atang.business@gmail.com>
2015-12-01 20:21:12 -0500

    added project files

---
+++ FITernitiData/Config/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace FITernitiData.Config
+{
+    public class Settings
+    {
+        private static string _connectionString;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString = WebConfigurationManager.ConnectionStrings["HealthBlogDB"].ConnectionString;
+                }
+
+                return _connectionString;
+            }
+        }
+    }
+}
---
+++ FITernitiMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FITernitiMVC.MvcApplication" Language="C#" %>
