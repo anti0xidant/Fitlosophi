d3ecb07b10b10c450364170bc60a6817a6d3dd2a
koshin abdi <kabdi8@gmail.com>
2015-12-02 13:58:36 -0500

    updated models part 2

--- FITlosophiData/Models/Post.cs
+++ FITlosophiData/Models/Post.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FITlosophiData.Models;
 
-namespace FITernitiMVC.Models
+namespace FITlosophiData.Models
+
 {
     public class Post
     {
--- FITlosophiData/Models/Tag.cs
+++ FITlosophiData/Models/Tag.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace FITernitiMVC.Models
+namespace FITlosophiData.Models
 {
     public class Tag
     {
