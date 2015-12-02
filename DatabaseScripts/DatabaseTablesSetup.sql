bc870a253b293c6debcae00ddbf5cab822506c1f
Andrew Tang <atang.business@gmail.com>
2015-12-02 10:19:29 -0500

    updated create table scripts

--- DatabaseScripts/DatabaseTablesSetup.sql
+++ DatabaseScripts/DatabaseTablesSetup.sql
@@ -1,17 +1,51 @@
---*************Uncomment to drop table *************
-	 --USE MASTER
-	 --GO
-	 --DROP DATABASE HealthBlogDB
-	 --GO
--- *************************************************
+--*************Step1 - run this to create database  *************
 
-USE MASTER
-GO
-CREATE DATABASE HealthBLogDB
+	USE MASTER
+	GO
+	CREATE DATABASE HealthBLogDB
 
+--*************************************************
+
+--*************Step2 - run this to create database  *************
+
+	-- Launch index page from MVC project, register an account
+	-- to create Identity tables
+
+--*************************************************
+
+--*************Step 3 - run this to drop table if they existing before step 4 *************
+
+	 USE HealthBlogDB
+	 GO
+
+	 DROP TABLE PostsXHash
+	 DROP TABLE Posts
+	 DROP TABLE Categories
+	 DROP TABLE HashTags 
+
+	 GO
+
+--*************Step 4 - run this to create remaining tables  *************
 USE HealthBlogDB
 GO
 
+
+
+CREATE TABLE Categories
+(
+	CategoryID		int IDENTITY(1,1)	NOT NULL	PRIMARY KEY,
+	CategoryName	nvarchar(100)		NOT NULL
+)
+GO
+
+
+
+CREATE TABLE HashTags
+(
+	TagID		int	IDENTITY(1,1)	NOT NULL	PRIMARY KEY,
+	TagName		nvarchar(1000)		NOT NULL
+)	
+GO
 
 
 CREATE TABLE Posts
@@ -32,24 +66,6 @@ CREATE TABLE Posts
 	CONSTRAINT FK_CategoryID	FOREIGN KEY (CategoryID)	REFERENCES	Categories	(CategoryID),
 	CONSTRAINT FK_UserID		FOREIGN KEY (UserID)		REFERENCES	AspNetUsers (Id)	
 ) 
-GO
-
-
-
-CREATE TABLE Categories
-(
-	CategoryID		int IDENTITY(1,1)	NOT NULL	PRIMARY KEY,
-	CategoryName	nvarchar(100)		NOT NULL
-)
-GO
-
-
-
-CREATE TABLE HashTags
-(
-	TagID		int	IDENTITY(1,1)	NOT NULL	PRIMARY KEY,
-	TagName		nvarchar(1000)		NOT NULL
-)	
 GO
 
 
