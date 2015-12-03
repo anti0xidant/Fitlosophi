6a8142726d7657852eb4d89e9d539d97d7721c32
koshin abdi <kabdi8@gmail.com>
2015-12-03 16:41:58 -0500

    added bll for read

--- FITlosophiBLL/FITlosophiOperations.cs
+++ FITlosophiBLL/FITlosophiOperations.cs
@@ -41,6 +41,73 @@ namespace FITlosophiBLL
         #region Read
 
 
+        public List<Post> GetAllPostSummaries()
+        {
+            var read = new Read();
+
+            var posts = read.GetAllPostSummaries();
+
+            return posts;
+        } 
+
+
+
+        public List<Post> GetAllPostsByCategory(int categoryID)
+        {
+            var read = new Read();
+            var posts = read.GetAllPostsByCategory(categoryID);
+
+            foreach (var p in posts)
+            {
+                p.HashTags = read.GetTagsByPostID(p.PostID);
+            }
+
+            return posts;
+        } 
+
+
+        public List<Post> GetPostsByAmount(int amount)
+        {
+            var read = new Read();
+            var posts = read.GetPostsByAmount(amount);
+
+            foreach (var p in posts)
+            {
+                p.HashTags = read.GetTagsByPostID(p.PostID);
+            }
+
+            return posts;
+        }
+
+
+
+
+        public List<Post> GetPostsByTagID(int tagID)
+        {
+            var read = new Read();
+            var posts = read.GetPostsByTagID(tagID);
+
+            foreach (var p in posts)
+            {
+                p.HashTags = read.GetTagsByPostID(p.PostID);
+            }
+
+            return posts;
+        }
+
+
+
+        public Post GetPostByID(int id)
+        {
+            var read = new Read();
+
+            var post = read.GetPostByID(id);
+
+            post.HashTags = read.GetTagsByPostID(post.PostID);
+
+            return post;
+        }
+
 
         #endregion
 
