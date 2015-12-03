36cbc5bcac39b5ade1bbdfb7da2c46712966928c
Mike Drozda <mdrozda@gmail.com>
2015-12-03 17:28:21 -0500

    setup views and controllers

--- FITlosophiMVC/Controllers/HomeController.cs
+++ FITlosophiMVC/Controllers/HomeController.cs
@@ -15,19 +15,11 @@ namespace FITlosophiMVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Test()
+        public ActionResult StaticPage()
         {
             return View();
         }
