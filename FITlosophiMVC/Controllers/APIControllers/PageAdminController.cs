150eeda925557790fff3b21e9af53680899fd115
Mike Drozda <mdrozda@gmail.com>
2015-12-05 14:22:53 -0500

    Manage pages is now loading data via Ajax and database

--- FITlosophiMVC/Controllers/APIControllers/PageAdminController.cs
+++ FITlosophiMVC/Controllers/APIControllers/PageAdminController.cs
@@ -4,10 +4,57 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using FITlosophiBLL;
+using FITlosophiData.Models;
 
 namespace FITlosophiMVC.Controllers.APIControllers
 {
     public class PageAdminController : ApiController
     {
+        #region Get
+
+        public List<StaticPage> GetAllPageSummaries()
+        {
+            var read = new FITlosophiOperations();
+
+            return read.GetAllPageSummaries();
+        }
+
+        public StaticPage GetPageByID(int PageID)
+        {
+            var read = new FITlosophiOperations();
+
+            return read.GetPageByID(PageID);
+        }
+
+        #endregion
+
+        #region Update
+
+        public void EditPage(StaticPage StaticPage)
+        {
+            var update = new FITlosophiOperations();
+
+           update.EditPage(StaticPage);
+        }
+
+        public void PublishPage(int StaticPageID)
+        {
+            var update = new FITlosophiOperations();
+
+            update.PublishPage(StaticPageID);
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void DeletePage(int StaticPageID)
+        {
+            var delete = new FITlosophiOperations();
+            delete.DeletePage(StaticPageID);
+        }
+
+        #endregion
     }
 }
