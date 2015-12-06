af6084f47a09592c6c573e26f0ffa6090536ae0f
Andrew Tang <atang.business@gmail.com>
2015-12-06 18:09:01 -0500

    began to implement hashtag autocomplete. fixed issue where HashTag object didnt' map with Sql because no TagName field, only ActualHashTag.

---
+++ FITlosophiMVC/Controllers/APIControllers/HashTagsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FITlosophiData.Models;
+using FITlosophiData.Repo;
+
+namespace FITlosophiMVC.Controllers.APIControllers
+{
+    public class HashTagsController : ApiController
+    {
+        public List<HashTag> Get()
+        {
+            var read = new Read();
+
+            return read.GetAllTags();
+        }
+    }
+}
--- FITlosophiMVC/Scripts/tagit/TagitInitializer.js
+++ FITlosophiMVC/Scripts/tagit/TagitInitializer.js
@@ -1,7 +1,20 @@
 ﻿$(document).ready(function() {
+    var x = [];
+    $.ajax({
+        url: 'api/HashTags/Get',
+        type: 'GET',
+        success: function(data, status, xhr) {
+            $.each(data, function(index, hashTag) {
+                x.Push(hashTag);
+            });
+        }
+    });
+
     $('#tagit').tagit({
         placeholderText: "Hashtags",
         allowSpaces: true,
-       
-});
+        availableTags: x
+    });
+
+
 });
