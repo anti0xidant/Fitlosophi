ed197dfb520504ae7a1b77825b1aa95ff7311b32
Andrew Tang <atang.business@gmail.com>
2015-12-07 11:53:18 -0500

    added coverimgurl field. implemented framework for add image modal

--- FITlosophiMVC/Scripts/app/AddPost.js
+++ FITlosophiMVC/Scripts/app/AddPost.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
     $.ajax({
-        url: '/api/Categories',
+        url: '/api/Categories/Get',
         type: 'GET',
         success: function(data, status, xhr) {
             $.each(data, function(index, category) {
@@ -18,4 +18,17 @@
             $('#PostCategoryID').css('color', '#555');
         }
     });
+
+    // When wants to upload image as CoverIMG
+    $('#CoverImgURL').click(function () {
+        // Display modal
+
+        $('#AddImageURL').click(function() {
+            $('#CoverImgURL').val($('#ImgURLHiddenInput').val());
+            // clear $('#ImgURLHiddenInput') value
+            // clear preview image
+            // close modal
+        });
+    });
+
 });
--- FITlosophiMVC/Scripts/tinymce/TinyMCEInitializer.js
+++ FITlosophiMVC/Scripts/tinymce/TinyMCEInitializer.js
@@ -5,10 +5,16 @@ $(document).ready(function() {
         selector: "#tinymce",
         menubar: false,
         skin: "andrew",
-        plugins: ["link, image, fullscreen preview"],
+        plugins: ["link, image, fullscreen, preview, imagetools"],
         toolbar1: "undo redo | bold italic underline strikethrough | subscript superscript | alignleft aligncenter alignright | numlist bullist | fullscreen",
         toolbar2: "removeformat | formatselect | fontselect | fontsizeselect | link image | preview",
-        content_css: "css/example.css"
+        tooldbar3: "imagetools",
+        content_css: "css/example.css",
+        image_advtab: true,
+        image_list: [
+            { title: "Dog", value: "https://i.ytimg.com/vi/KY4IzMcjX3Y/maxresdefault.jpg" },
+            { title: "Cat", value: "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg" }
+        ]
     });
 
 });
