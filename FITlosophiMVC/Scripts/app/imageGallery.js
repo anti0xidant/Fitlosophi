3649509ae37b9fadd3441382d78f469193236768
Mike Drozda <mdrozda@gmail.com>
2015-12-09 17:34:51 -0500

    Added image gallery to add and edit page/posts

--- FITlosophiMVC/Scripts/app/imageGallery.js
+++ FITlosophiMVC/Scripts/app/imageGallery.js
@@ -10,16 +10,26 @@
             $.each(data.resources, function (index, image) {
                 $(addImageToGallery(image, index)).appendTo($('#blogImageGallery'));
             });
+            
+            $('.imageToCopy').on("click", function () {
+                var imageInsertURL = $(this).children('input').eq(0).val();
+                var x = '<img src="' + imageInsertURL + '"/>';
+                tinymce.get("tinymce").execCommand('mceInsertContent', false, x);
+            });
+
         },
         error: function (xhr, status, err) {
             alert('error:' + err);
         }
     });
+
+   
 });
 
 function addImageToGallery(image, index) {
 
     var url = "http://res.cloudinary.com/dk0wmifkq/image/upload/w_200,h_150,c_fit/v" + image.version + '/' + image.public_id + '.' + image.format;
-    return '<li><a href="#" title="Image" data-subtitle="Click to Add" id="imageToCopy"><img src="' + url + '"></a></li>';
+    var value = "http://res.cloudinary.com/dk0wmifkq/image/upload/v" + image.version + '/' + image.public_id + '.' + image.format;
+    return '<li><a href="#" title="Image" data-subtitle="Click to Add" class="imageToCopy"><input type="hidden" value="' + value + '"/><img src="' + url + '"></a></li>';
 }
 
