6283b34d46180b24c717494ca998434b3ac17773
Andrew Tang <atang.business@gmail.com>
2015-12-16 22:12:35 -0600

    replaced missing }); from managePosts.js which was causing Manage Posts table to not show

--- FITlosophiMVC/Scripts/app/managePosts.js
+++ FITlosophiMVC/Scripts/app/managePosts.js
@@ -56,75 +56,74 @@
     });
 
 
-//    $('#managePosts').on('click', '.btnPublishPost', function () {
-//        $('#PublishModalPostID').val($(this).val());
-//        $('#PublishModalBodyTitle').html('Publish:' + $(this).closest("tr").children("td").eq(2).html());
-//        $('#PublishModal').modal('show');
+    //    $('#managePosts').on('click', '.btnPublishPost', function () {
+    //        $('#PublishModalPostID').val($(this).val());
+    //        $('#PublishModalBodyTitle').html('Publish:' + $(this).closest("tr").children("td").eq(2).html());
+    //        $('#PublishModal').modal('show');
 
-//        //Remove any click events from Confirm button, setup confirm buttton for publish event
-//        $('#PublishConfirm').unbind();
-//        $('#PublishConfirm').click(function () {
-   
-//            var post = {};
-//            post.PostID = $('#PublishModalPostID').val();
-//            post.HasSchedule = $('.HasSchedule:checked').val();
-//            post.StartDate = $('#FromDate').val();
-//            post.EndDate = $('#ToDate').val();
+    //        //Remove any click events from Confirm button, setup confirm buttton for publish event
+    //        $('#PublishConfirm').unbind();
+    //        $('#PublishConfirm').click(function () {
 
-//            $.ajax({
-//                url: '/api/PostsAdmin/PublishPostWithSchedule',
-//                type: 'PUT',
-//                data: post,
-//                success: function (data, status, xhr) {
-//                    $('#PublishModal').modal('hide');
-//                    loadPosts();
-//                },
-//                error: function (xhr, status, err) {
-//                    alert('error:' + err);
-//                }
-//            });
-//        });
-//    });
-//});
+    //            var post = {};
+    //            post.PostID = $('#PublishModalPostID').val();
+    //            post.HasSchedule = $('.HasSchedule:checked').val();
+    //            post.StartDate = $('#FromDate').val();
+    //            post.EndDate = $('#ToDate').val();
 
-/* Uses the  */
-function loadPosts() {
-    $.ajax({
-        url: '/api/PostsAdmin/GetAllPostSummaries',
-        type: 'GET',
-        success: function (data, status, xhr) {
-            $('#managePosts').empty();
+    //            $.ajax({
+    //                url: '/api/PostsAdmin/PublishPostWithSchedule',
+    //                type: 'PUT',
+    //                data: post,
+    //                success: function (data, status, xhr) {
+    //                    $('#PublishModal').modal('hide');
+    //                    loadPosts();
+    //                },
+    //                error: function (xhr, status, err) {
+    //                    alert('error:' + err);
+    //                }
+    //            });
+    //        });
+    //    });
+    //});
 
-            $.each(data, function (index, post) {
-                $(createTableDataManagePosts(post, index)).appendTo($('#managePosts'));
-            });
+    /* Uses the  */
+    function loadPosts() {
+        $.ajax({
+            url: '/api/PostsAdmin/GetAllPostSummaries',
+            type: 'GET',
+            success: function (data, status, xhr) {
+                $('#managePosts').empty();
+
+                $.each(data, function (index, post) {
+                    $(createTableDataManagePosts(post, index)).appendTo($('#managePosts'));
+                });
+            }
+        });
+    };
+
+
+
+    // Creates HTML table row of player data which is used to populate Roster Table in loadRoster()
+    function createTableDataManagePosts(post, index) {
+
+        if (post.IsActive) {
+            //var dateCreated = $.format.date(post.DateCreated, "dd~MM~yyyy");
+            var datePublished = '';
+            var publishBtn = '';
+
+            //Display the published date if it has been published, if not create a publish button
+            if (post.IsPublished) {
+                datePublished = post.DatePublished;
+            } else {
+                publishBtn = '<button class=\"btn btn-tales-one btn-xs btnPublishPost\" value=\"' + post.PostID + '\">Publish</button>';
+            }
+
+            return '<tr><td>' + (index + 1) + '</td><td>' + post.DateCreated + '</td><td>' + post.Title + '</td><td>' + post.WriterName + '</td><td>' + datePublished + '</td></td><td><a class=\"btn btn-tales-two btn-xs btnEditPost\" href="/CMS/EditPost?id=' + post.PostID + '\">Edit Post</a></td>' +
+                '<td><button class=\"btn btn-danger btn-xs btnDeletePost\" value=\"' + post.PostID + '\">Delete</button></td><td>' + publishBtn + '</td></tr>';
         }
-    });
-};
-
-
-
-// Creates HTML table row of player data which is used to populate Roster Table in loadRoster()
-function createTableDataManagePosts(post, index) {
-
-    if (post.IsActive) {
-        //var dateCreated = $.format.date(post.DateCreated, "dd~MM~yyyy");
-        var datePublished = '';
-        var publishBtn = '';
-
-        //Display the published date if it has been published, if not create a publish button
-        if (post.IsPublished) {
-            datePublished = post.DatePublished;
-        } else {
-            publishBtn = '<button class=\"btn btn-tales-one btn-xs btnPublishPost\" value=\"' + post.PostID + '\">Publish</button>';
-        }
-
-        return '<tr><td>' + (index + 1) + '</td><td>' + post.DateCreated + '</td><td>' + post.Title + '</td><td>' + post.WriterName + '</td><td>' + datePublished + '</td></td><td><a class=\"btn btn-tales-two btn-xs btnEditPost\" href="/CMS/EditPost?id=' + post.PostID + '\">Edit Post</a></td>' +
-            '<td><button class=\"btn btn-danger btn-xs btnDeletePost\" value=\"' + post.PostID + '\">Delete</button></td><td>' + publishBtn + '</td></tr>';
-    }
-}
-
-
+    };
+});
 
 
 
